b93dc83db43ecb52f870283edfbbca629de0d831
Amitava Karan <amkaran@microsoft.com>
2024-07-31 23:03:55 +0530

    Updated code

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
 class App {
   #sessionId = null;
   clipboardPermissionState = null;
-  pasteOption = this.#pastePasteOptionDefault;
-  lastCopyPasteType = this.#notAvailable;
+  lastCopyPasteType = null;
 
   constructor() {}
 
   run() {
     this.#UpdateSessionId();
 
-    this.#initializeAppSettings();
-
     this.#UpdateClipboardPermissionState();
+
+    this.#registerEventListeners();
 
     this.#logMessage("App is running");
   }
-
-  #initializeAppSettings() {}
 
   #UpdateSessionId() {
     const sessionIdElm = document.getElementById("session-id");
@@ -66,6 +63,113 @@ class App {
     }
 
     return clipboardPermissionState;
+  }
+
+  #registerEventListeners() {
+    this.#registerCopyEventListeners();
+  }
+
+  #registerCopyEventListeners() {
+    document.addEventListener(
+      "keydown",
+      (e) => {
+        console.log("Keydown event detected!");
+
+        if (e.key === "c" && e.ctrlKey) {
+          console.log("Ctrl + C key combination detected!");
+
+          this.#logMessage("*** Copy operation initiated (CTRL + C) ***");
+
+          this.#performCopyOperation(true);
+        }
+      },
+      { capture: true }
+    );
+
+    const copyBtnElm = document.getElementById("copy-btn");
+    copyBtnElm.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      this.#logMessage(
+        "*** Copy operation initiated (copy button is clicked) ***"
+      );
+
+      this.#performCopyOperation(false);
+    });
+  }
+
+  #performCopyOperation(isKeyboardEvent) {
+    const dataFormats = {
+      ["text/plain"]: "Retrieving data from server. Please wait...",
+    };
+
+    this.#writeToClipboard(isKeyboardEvent, dataFormats);
+  }
+
+  #registerOnDemandCopyEventListener(writeDataToClipboard) {
+    const copyEventListener = (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      document.removeEventListener("copy", copyEventListener);
+
+      this.#logMessage("Copy event detected");
+
+      writeDataToClipboard(e);
+    };
+
+    document.addEventListener("copy", copyEventListener);
+  }
+
+  async #writeToClipboard(isKeyboardEvent, dataFormats) {
+    if (isKeyboardEvent) {
+      this.#registerOnDemandCopyEventListener((e) => {
+        this.#writeToClipboardFactory(e, dataFormats);
+      });
+    } else {
+      if (this.clipboardApi === this.#dataTransferApi) {
+        this.#registerOnDemandCopyEventListener((e) => {
+          this.#writeToClipboardFactory(e, dataFormats);
+        });
+        const execCommandResult = document.execCommand("copy");
+        this.#logMessage(`execCommand('copy') result: '${execCommandResult}'`);
+      } else if (this.clipboardApi === this.#asyncClipboardApi) {
+        await this.#writeToClipboardAsyncApi(dataFormats);
+      }
+    }
+  }
+
+  #writeToClipboardFactory(e, dataFormats) {
+    if (this.clipboardApi === this.#dataTransferApi) {
+      this.#writeToClipboardDataTransferApi(e, dataFormats);
+    } else if (this.clipboardApi === this.#asyncClipboardApi) {
+      this.#writeToClipboardAsyncApi(dataFormats);
+    }
+  }
+
+  #writeToClipboardDataTransferApi(e, dataFormats) {
+    for (const [mimeType, copyPayload] of Object.entries(dataFormats)) {
+      try {
+        e.clipboardData.setData(mimeType, copyPayload);
+      } catch (err) {
+        this.#logMessage(
+          `Failed to write data of type: '${mimeType}' in clipboard`
+        );
+      }
+    }
+  }
+
+  async #writeToClipboardAsyncApi(dataFormats) {
+    if (!navigator.clipboard) {
+      this.#logMessage("Clipboard API is not supported");
+      return;
+    }
+
+    try {
+      const clipboardItem = new ClipboardItem(dataFormats);
+      await navigator.clipboard.write([clipboardItem]);
+    } catch (err) {
+      this.#logMessage("Failed to write data in clipboard using Async API");
+    }
   }
 
   #generateUUID() {
@@ -132,6 +236,16 @@ class App {
     return copyPayloads;
   }
 
+  get addDelayToCopy() {
+    const addDelayToCopyElm = document.getElementById("add-delay-chkbox");
+    return addDelayToCopyElm.checked;
+  }
+
+  get pasteOption() {
+    const pasteOptionElm = document.getElementById("paste-option");
+    return pasteOptionElm.value;
+  }
+
   get #notAvailable() {
     return "NA";
   }
@@ -170,6 +284,18 @@ class App {
 
   get #pastePasteOptionDefault() {
     return "default";
+  }
+
+  get #pastePasteOptionHTML() {
+    return "pasteHtml";
+  }
+
+  get #pastePasteOptionImage() {
+    return "pasteImage";
+  }
+
+  get #pastePasteOptionWebCustomFormat() {
+    return "pasteWCF";
   }
 }
 
